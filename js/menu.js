0490ec1493249fd0f8a43f68546dd01a0f7f891f
InM1nd <zabolotny1111@gmail.com>
2024-02-24 22:46:18 +0200

    menu fix

--- js/menu.js
+++ js/menu.js
@@ -8,3 +8,11 @@ document.getElementById('closeBtn').addEventListener('click', function() {
   document.body.style.overflow = '';
 });
 
+var menuLinks = document.querySelectorAll('#menu a');
+
+menuLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    document.getElementById('menu').classList.remove('active');
+    document.body.style.overflow = '';
+  });
+});
