d762c4e944c57ebe3bed5c78a1f8b2a8558e093f
InM1nd <zabolotny1111@gmail.com>
2024-05-07 12:47:13 +0200

    213

--- js/swiper.js
+++ js/swiper.js
@@ -1,1 +1,14 @@
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.1,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1.1,
+  breakpoints: {
+    // when window width is >= 768px (desktop)
+    801: {
+      slidesPerView: 4.1,
+    },
+  },
+});
